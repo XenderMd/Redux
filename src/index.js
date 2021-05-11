76249788443e0727a6d27a950108444090183f5a
XenderMd <denis.stavilamd@gmail.com>
2021-05-11 14:49:36 +0200

    Communicating with props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
+// Import React and ReactDOM libraries
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+import faker from 'faker';
+
+if (module.hot) {
+    module.hot.accept();
+  }
+
+
+//Create react component
+const App = function(){
+
+    return (
+        <div className="ui container comments">
+            
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45pm" image={faker.image.avatar()} commentText="Awesome !!!"/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:00pm" image={faker.image.avatar()} commentText="Best idea of the day !!!"/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00pm" image={faker.image.avatar()} commentText="Can't wait to find out more!"/>
+            </ApprovalCard>
+            
+        </div>
+    );
+};
+
+
+
+// Take the react component and show it on the screen 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
