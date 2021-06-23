815eddebe73f6e46c253391c50cdbd83fda35b46
XenderMd <denis.stavilamd@gmail.com>
2021-06-23 18:00:19 +0200

    Implemented SPA navigation from scratch

--- src/App.js
+++ src/App.js
@@ -4,13 +4,63 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Header from './components/Header';
+import Route from './components/Route';
+
+const accordionItems=[
+    {
+        title:'What is React?',
+        content:"React is a front end javascript framework"    
+    },
+    {
+        title:"Why use react?",
+        content:'React is a favorite JS library among JS engineers'
+    },
+    {
+        title:'How do you use React?',
+        content:'You use React by creating components'
+    }
+];
+
+const dropdownItems=[
+    {
+        label:"The color Red",
+        value:'red'   
+    },
+    {
+        label:"The color Green",
+        value:'green'
+    },
+    {
+        label:"A shade of Blue",
+        value:'blue'
+    }
+];
 
 
 const App = () => {
 
+    const [selected, setSelected]=useState(dropdownItems[0]);
+
     return(
         <div>
-            <Translate/>
+            <Header></Header>
+            <Route path="/">                
+                <Accordion items={accordionItems}/>
+            </Route>
+            <Route path="/list">                
+                <Search/>
+            </Route>
+            <Route path="/dropdown">                
+                <Dropdown label="Select a color"
+                          selected={selected} 
+                          onSelectedChange={setSelected}
+                          options={dropdownItems}
+                          />
+            </Route>
+            <Route path="/translate">                
+                <Translate/>
+            </Route>
         </div>
     );
 }
