21fb16aa8e52493ddb1728f04ff1cf952a5619d4
XenderMd <denis.stavilamd@gmail.com>
2021-09-21 16:00:35 +0200

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
-// Import React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import App from './components/App';
 
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
-
-import faker from 'faker';
-
-if (module.hot) {
-    module.hot.accept();
-  }
-
-
-//Create react component
-const App = function(){
-
-    return (
-        <div className="ui container comments">
-            
-            <ApprovalCard>
-                <CommentDetail author="Sam" timeAgo="Today at 4:45pm" image={faker.image.avatar()} commentText="Awesome !!!"/>
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail author="Alex" timeAgo="Today at 2:00pm" image={faker.image.avatar()} commentText="Best idea of the day !!!"/>
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00pm" image={faker.image.avatar()} commentText="Can't wait to find out more!"/>
-            </ApprovalCard>
-            
-        </div>
-    );
-};
-
-
-
-// Take the react component and show it on the screen 
 ReactDOM.render(
     <App/>,
-    document.querySelector('#root')
-);
+    document.querySelector("#root")
+)
