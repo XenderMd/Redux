aaec6a46c62a0664990293e9b0e14324a0b07957
XenderMd <denis.stavilamd@gmail.com>
2021-10-20 07:46:51 +0200

    Implemented setup

--- src/index.js
+++ src/index.js
@@ -3,37 +3,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
+import App from './components/App'
 
-import faker from 'faker';
-
-if (module.hot) {
-    module.hot.accept();
-  }
-
-
-//Create react component
-const App = function(){
-
-    return (
-        <div className="ui container comments">
-            
-            <ApprovalCard>
-                <CommentDetail author="Sam" timeAgo="Today at 4:45pm" image={faker.image.avatar()} commentText="Awesome !!!"/>
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail author="Alex" timeAgo="Today at 2:00pm" image={faker.image.avatar()} commentText="Best idea of the day !!!"/>
-            </ApprovalCard>
-
-            <ApprovalCard>
-                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00pm" image={faker.image.avatar()} commentText="Can't wait to find out more!"/>
-            </ApprovalCard>
-            
-        </div>
-    );
-};
 
 
 
